f08308ad2e4e1a553181feb80392be28763bf9a1
e-rojas <109676581+brightdevs@users.noreply.github.com>
2022-07-26 16:13:14 -0300

    chore: header component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BsHouse, BsHeart, BsBell } from 'react-icons/bs';
+
+const Header = () => {
+  return (
+    <header className='navigation'>
+      <div className='navigation__brand'>
+        <BsHouse className='navigation__brand__icon' />
+        <span className='navigation__brand__name'>tripper</span>
+      </div>
+      <div className='navigation__nav'>
+        <ul>
+          <li>
+            <a href='none'>Accomodations</a>
+          </li>
+          <li>
+            <a href='nond'>Car rentals</a>
+          </li>
+          <li>
+            <a href='nond'>Flights</a>
+          </li>
+        </ul>
+      </div>
+      <div className='navigation__info'>
+        <BsHeart className='navigation__info__icon' />
+        <BsBell className='navigation__info__icon' />
+        <img
+          src='https://avatars.githubusercontent.com/u/109676581?v=4'
+          alt='profile'
+          className='navigation__info__profile'
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
