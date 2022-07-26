64482e64f8da24ae1df84dc41b017e346d53ad60
e-rojas <109676581+brightdevs@users.noreply.github.com>
2022-07-26 19:48:29 -0300

    clean up css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,34 +12,15 @@ const Home = () => {
             <SmartInput placeHolder='Location' label='Location'>
               <BiMap className='icon' />
             </SmartInput>
-            {/* </div> */}
-            <div className='section'>
-              <input type='text' />
-            </div>
-            <div className='section'>
-              <input type='text' />
-            </div>
-            <div className='section'>
-              <input type='text' />
-            </div>
-            {/* <div className='locations__search-locations'>
-              <div className='section-wrapper'>
-                <BiMap className='locations__search-locations__icon' />
-                <div className='input-wrapper'>
-                  <span className='label'>Location</span>
-                  <input type='text' placeholder='Moncton, NB' />
-                </div>
-              </div>
-            </div> */}
-            {/* <div className='locations__search-date'>
-              <input type='text' placeholder='Tuesday 4' />
-            </div>
-            <div className='locations__search-date'>
-              <input type='text' placeholder='Friday 6' />
-            </div>
-            <div className='locations__search-guest'>
-              <input type='text' placeholder='2 Rooms' />
-            </div> */}
+            <SmartInput placeHolder='Location' label='Location'>
+              <BiMap className='icon' />
+            </SmartInput>
+            <SmartInput placeHolder='Location' label='Location'>
+              <BiMap className='icon' />
+            </SmartInput>
+            <SmartInput placeHolder='Location' label='Location'>
+              <BiMap className='icon' />
+            </SmartInput>
           </div>
           <h1>locations</h1>
         </div>
